ab0922ac566bbd0f1a633dd48b4113aab1a86104
FabrizioGomes <Desk.7016@URGTEC.local>
2021-05-12 16:01:30 -0300

    First Commit

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,20 @@
+// uma divisao por 0 ao inves de gerar erro converte para infinito
+console.log(7 / 0)
+/*
+ dessa forma reconhece que seja numero quando é um inteiro,
+ porem quando inserido um float nao reconhece
+*/
+console.log("10" / 2)
+
+// quando é uma String o js nao realiza a função
+console.log("Show!" * 2)
+
+// js nao é tao preciso na conversao
+console.log(0.1 + 0.7)
+
+//console.log(10.toString()) nao funciona
+
+
+console.log((10.345).toFixed(2))
+
+
